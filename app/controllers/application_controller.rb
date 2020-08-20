3d4973dc483d0d361877a27cb82e5d09c64e55f6
Isyraf Zakil <isyrafimrani@gmail.com>
2020-08-19 23:10:26 -0400

    Devise gem setup for auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::Base
+
+	# def current_user
+ #        user_id = cookies[:user_id]
+ #        if user_id
+ #            begin
+ #                @current_user = User.find(user_id)
+ #            rescue ActiveRecord::RecordNotFound
+ #                redirect_to new_user_path, flash: {warning: "User not found, Create a user!"}                
+ #            end
+ #        else
+ #            redirect_to sign_in_users_path, flash: {info: "Please Login!"}
+ #        end
+        
+ #    end
+
+ 	before_action :configure_permitted_parameters, if: :devise_controller?
+
+  	protected
+
+	  def configure_permitted_parameters
+	    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+	  end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 include UsersHelper
 
+  before_action :authenticate_user!
+  
   def new
   	@user = User.new
   end
